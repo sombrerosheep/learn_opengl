0981fe42e4104c67e0184c9c75aa60652b7bf260
sombrerosheep <sombrerosheep@gmail.com>
2019-12-24 16:46:53 -0600

    update build bat

--- winbuild.bat
+++ winbuild.bat
@@ -1,26 +1,46 @@
 @echo off
 
+REM Clear all args to 0
 SET CLEANBUILD=0
 SET SETUPENV=0
+SET REBUILD=0
 
+REM Process args
+:ARGS
+IF "%1"=="" GOTO EXEC
 IF "%1"=="CLEAN" SET CLEANBUILD=1
-IF "%2"=="CLEAN" SET CLEANBUILD=1
+IF "%1"=="SETENV" SET SETUPENV=1
+IF "%1"=="REBUILD" SET REBUILD=1
+SHIFT
+
+GOTO ARGS
+
+
+REM execute actions
+:EXEC
 
 IF "%CLEANBUILD%"=="1" (
   echo "cleaning build dir"
   DEL /S .\build\* /q
+  SET REBUILD=1
 )
-
-IF "%1"=="SETENV" SET SETUPENV=1
-IF "%2"=="SETENV" SET SETUPENV=1
 
 IF "%SETUPENV%"=="1" (
   echo "init vs env"
   @call "%vs150comntools%VsDevCmd.bat"
 )
-mkdir build
+
+IF NOT EXIST .\build (
+  mkdir build
+)
+
 pushd .\build
-cmake ..\ -G "Visual Studio 15 2017 Win64"
+
+IF "%REBUILD%"=="1" (
+  echo "rebuilding cmake project"
+  cmake ..\ -G "Visual Studio 15 2017 Win64"
+)
+
 devenv learn-opengl.sln /build
 
 popd
