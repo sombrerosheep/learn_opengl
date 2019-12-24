ecc45be3ba146b8a5bdd546438d9eb05ff343e70
sombrerosheep <sombrerosheep@gmail.com>
2019-12-24 12:04:18 -0600

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char** argv) {
+  printf("Hello World\n");
+  /*
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+  if (window == NULL) {
+    printf("Failed to create GLFW window\n");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  */
+  
+  return 0;
+}
