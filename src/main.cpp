af4904ec18382cfd9a4a7eea93c2075e12e63b61
sombrerosheep <sombrerosheep@gmail.com>
2019-12-27 12:39:42 -0600

    update shader
    add support for vector and matrix uniforms

--- src/main.cpp
+++ src/main.cpp
@@ -158,9 +158,8 @@ int main(int argc, char** argv) {
     glm::mat4 trans = glm::mat4(1.0f);
     trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
     trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    ourShader.setMat4("transform", trans);
 
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
