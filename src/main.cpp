b6a9192d01378830cdbd3a15f2b7da6ef4494eea
sombrerosheep <sombrerosheep@gmail.com>
2019-12-26 21:36:05 -0600

    textures :: exerceise 2

--- src/main.cpp
+++ src/main.cpp
@@ -45,10 +45,10 @@ int main(int argc, char** argv) {
   Shader ourShader("shaders/basic/shader.vert", "shaders/basic/shader.frag");
 
   float vertices[] = {
-    0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 0.0f,      1.0f, 1.0f,
-    0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,      1.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,      0.0f, 0.0f,
-    -0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 0.0f,      0.0f, 1.0f
+    0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 0.0f,      2.0f, 2.0f, // top-right
+    0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,      2.0f, 0.0f, // bot-right
+    -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,      0.0f, 0.0f, // bot-left
+    -0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 0.0f,      0.0f, 2.0f  // top-left
   };
   unsigned int indices[] = {
     0, 1, 3,
@@ -79,8 +79,8 @@ int main(int argc, char** argv) {
   glGenTextures(1, &texture1);
   glBindTexture(GL_TEXTURE_2D, texture1);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
