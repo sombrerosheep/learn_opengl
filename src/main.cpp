c729ea88da515afc73c1f366b65244073fa222c5
sombrerosheep <sombrerosheep@gmail.com>
2019-12-27 11:42:36 -0600

    matrix animation

--- src/main.cpp
+++ src/main.cpp
@@ -128,10 +128,6 @@ int main(int argc, char** argv) {
   }
   stbi_image_free(data);
 
-  glm::mat4 trans = glm::mat4(1.0f);
-  trans = glm::rotate(trans, glm::half_pi<float>(), glm::vec3(0.0, 0.0, 1.0));
-  trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-  
   ourShader.use();
   ourShader.setInt("texture1", 0);
   ourShader.setInt("texture2", 1);
@@ -158,6 +154,11 @@ int main(int argc, char** argv) {
 
     ourShader.use();
     ourShader.setFloat("blend", blend);
+
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
     unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
     glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
     glBindVertexArray(VAO);
