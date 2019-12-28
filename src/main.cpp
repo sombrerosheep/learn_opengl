4b85e262289c30e936cb943006fdd07835216b68
sombrerosheep <sombrerosheep@gmail.com>
2019-12-27 18:04:06 -0600

    many cubes
    end coordinate system

--- src/main.cpp
+++ src/main.cpp
@@ -62,6 +62,19 @@ int main(int argc, char** argv) {
 
   Shader ourShader("shaders/basic/shader.vert", "shaders/basic/shader.frag");
 
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+  };
+  
   float vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
      0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -169,7 +182,7 @@ int main(int argc, char** argv) {
   view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
   // projection matrix for the camera
   glm::mat4 projection;
-  projection = glm::perspective(glm::radians(45.0f), (float)(screenWidth / screenHeight), 0.1f, 100.0f);
+  projection = glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
 
   glEnable(GL_DEPTH_TEST);
   
@@ -191,17 +204,22 @@ int main(int argc, char** argv) {
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-  
     ourShader.use();
     ourShader.setFloat("blend", blend);
-    ourShader.setMat4("model", model);
     ourShader.setMat4("view", view);
     ourShader.setMat4("projection", projection);
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    
+    for (unsigned int i = 0; i < 10; i++) {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.f * i;
+      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+      ourShader.setMat4("model", model);
+
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     glfwSwapBuffers(window);
     glfwPollEvents();
