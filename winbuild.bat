18a209023691960aebe31d51ea3b7a7cd24e4647
sombrerosheep <sombrerosheep@gmail.com>
2021-12-22 11:55:46 -0600

    update winbuild

--- winbuild.bat
+++ winbuild.bat
@@ -9,7 +9,7 @@ REM Process args
 :ARGS
 IF "%1"=="" GOTO EXEC
 IF "%1"=="CLEAN" SET CLEANBUILD=1
-IF "%1"=="SETENV" SET SETUPENV=1
+IF "%1"=="SETUP" SET SETUPENV=1
 IF "%1"=="REBUILD" SET REBUILD=1
 SHIFT
 
@@ -25,10 +25,39 @@ IF "%CLEANBUILD%"=="1" (
   SET REBUILD=1
 )
 
-IF "%SETUPENV%"=="1" (
-  echo "init vs env"
-  @call "%vs150comntools%VsDevCmd.bat"
+IF %SETUPENV%==1 (
+  echo Initializing visual studio environment
+  SET foundvs=0
+
+  for %%a in (
+    "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
+    "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat"
+  ) do (
+    echo Looking for visual studio install at: %%a
+
+    IF EXIST "%%a" (
+      echo Found visual studio at: %%a
+
+      IF NOT defined DevEnvDir (
+        @call %%a x64
+      ) ELSE (
+        echo DevEnvDir already set. skipping call to vcvarsall.
+      )
+
+      SET foundvs=1
+      GOTO EXITVSLOOP
+    )
+  )
+
+  :EXITVSLOOP
+  IF "%foundvs%"=="0" (
+    echo ERROR - Couldn't find visual studio installation
+    echo ERROR - Failure setting up environment!
+  ) ELSE (
+    echo VS Setup complete
+  )
 )
+
 
 IF NOT EXIST .\build (
   mkdir build
@@ -41,6 +70,11 @@ IF "%REBUILD%"=="1" (
   cmake ..\ -G "Visual Studio 15 2017 Win64"
 )
 
-devenv learn-opengl.sln /build
+IF NOT defined DevEnvDir (
+  echo ERROR - DevEnvDir was not defined. Please re-run with the SETUP arg.
+) ELSE (
+  echo Building solution...
+  devenv learn-opengl.sln /build
+)
 
 popd
